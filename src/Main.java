26ef481fb1b0bd107bfb0b31f5d9cedd786d72c4
jaslyn-j <jaslynjacob026@gmail.com>
2026-03-27 14:42:03 +0530

    UC6

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,21 @@ import java.util.*;
 
 /**
  * author: Jaslyn
- * version: 4.0
+ * version: 6.0
  */
 
 public class Main {
     public static void main(String args[]){
         System.out.println("Train Consist Management App: ");
 
-        Set<String> formation = new LinkedHashSet<>();
-        formation.add("Engine");
-        formation.add("Sleeper");
-        formation.add("Cargo");
-        formation.add("Guard");
-        formation.add("Engine"); //testing duplicate property
-        System.out.println("Final Train Formation: " + formation);
+        Map<String, Integer> capacityMap= new HashMap<>();
+        capacityMap.put("First Class", 24);
+        capacityMap.put("Cargo", 120);
+        capacityMap.put("Sleeper", 72);
+        capacityMap.put("AC Chair", 56);
+
+        for(Map.Entry<String, Integer> entry : capacityMap.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
     }
 }
