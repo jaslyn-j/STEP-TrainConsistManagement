402cbe365379912982c9eb5e81500a6e624a5953
jaslyn-j <jaslynjacob026@gmail.com>
2026-03-27 14:17:37 +0530

    UC2

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,24 @@ import java.util.*;
 
 /**
  * author: Jaslyn
- * versino: 1.0
+ * versino: 2.0
  */
 
 public class Main {
     public static void main(String args[]){
-        System.out.println("--Train Consist Management App--");
-        int count=0;
+        System.out.println("Train Consist Management App: ");
 
-        List<String> trainConsist = new ArrayList<>();
+        List<String> passengerBogies=new ArrayList<>();
 
-        System.out.println("Train initialized successfully");
-        System.out.println("Inital Bogie count: "+count);
-        System.out.println("Current Train consist: "+trainConsist);
-        System.out.println("\nSystem ready for operations");
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+        System.out.println("After adding bogies: ");
+        System.out.println("Passenger Bogies: "+passengerBogies);
+        passengerBogies.remove("AC Chair");
+        System.out.println("\nAfter removing AC Chair: ");
+        System.out.println("Passenger Bogies: "+passengerBogies);
+        System.out.println("\nChecking if 'Sleeper' exists:\nContains Sleeper? "+passengerBogies.contains( "Sleeper"));
+        System.out.println("\nFinal train consist: "+passengerBogies);
     }
 }
