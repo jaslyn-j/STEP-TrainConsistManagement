d33f6ec2cbbaf0a57ef9258c3d4be14a02276ac0
jaslyn-j <jaslynjacob026@gmail.com>
2026-03-27 15:09:11 +0530

    UC9

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.stream.Collectors;
 
 /**
  * author: Jaslyn
- * version: 6.0
+ * version: 9.0
  */
 class Bogie {
     String name;
@@ -25,17 +25,21 @@ public class Main {
         bogies.add(new Bogie("AC Chair", 56));
         bogies.add(new Bogie("First Class", 40));
         bogies.add(new Bogie("General", 90));
+        bogies.add(new Bogie("Sleeper", 72)); // duplicate type for grouping demo
+        bogies.add(new Bogie("AC Chair", 56));
         System.out.println("\nAll Bogies:");
         for (Bogie b : bogies) {
             System.out.println(b);
         }
-        List<Bogie> filteredBogies = bogies
+        Map<String, List<Bogie>> groupedBogies = bogies
                 .stream()
-                .filter(b -> b.capacity > 60)
-                .collect(Collectors.toList());
-        System.out.println("\nFiltered Bogies (Capacity > 60):");
-        for (Bogie b : filteredBogies) {
-            System.out.println(b);
+                .collect(Collectors.groupingBy(b -> b.name));
+        System.out.println("\nGrouped Bogies By Type:");
+        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
+            System.out.println("\nBogie Type: " + entry.getKey());
+            for (Bogie b : entry.getValue()) {
+                System.out.println("  " + b);
+            }
         }
     }
 }
