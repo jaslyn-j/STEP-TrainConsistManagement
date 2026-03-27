961573022e29462cbcf48f9bfbcfd633a426bcb0
jaslyn-j <jaslynjacob026@gmail.com>
2026-03-27 14:25:23 +0530

    UC3

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,26 @@ import java.util.*;
 
 /**
  * author: Jaslyn
- * versino: 2.0
+ * version: 3.0
  */
 
 public class Main {
     public static void main(String args[]){
         System.out.println("Train Consist Management App: ");
 
-        List<String> passengerBogies=new ArrayList<>();
+        Set<String>  bogies= new HashSet<String>();
 
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("AC Chair");
-        passengerBogies.add("First Class");
-        System.out.println("After adding bogies: ");
-        System.out.println("Passenger Bogies: "+passengerBogies);
-        passengerBogies.remove("AC Chair");
-        System.out.println("\nAfter removing AC Chair: ");
-        System.out.println("Passenger Bogies: "+passengerBogies);
-        System.out.println("\nChecking if 'Sleeper' exists:\nContains Sleeper? "+passengerBogies.contains( "Sleeper"));
-        System.out.println("\nFinal train consist: "+passengerBogies);
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+        //duplicate entries test
+        bogies.add("BG101");
+        bogies.add("BG102");
+
+        System.out.println("Bogie IDs after insertion:\n" + bogies);
+
+        System.out.println("\nNote:\nDuplicates are automatically ignore by hashset");
+
     }
 }
