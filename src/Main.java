bf2dbbe24a72abfcc9935019f79f35ca27c9ae59
jaslyn-j <jaslynjacob026@gmail.com>
2026-03-27 13:59:34 +0530

    UC1: APP ENTRY

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/**
+ * author: Jaslyn
+ * versino: 1.0
+ */
+
+public class Main {
+    public static void main(String args[]){
+        System.out.println("--Train Consist Management App--");
+        int count=0;
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train initialized successfully");
+        System.out.println("Inital Bogie count: "+count);
+        System.out.println("Current Train consist: "+trainConsist);
+        System.out.println("\nSystem ready for operations");
+    }
+}
